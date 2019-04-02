89a12f3189e2fd75f2972ce8ffc4b1f5662f93ba
MailsonD <mailssondennis@gmail.com>
2019-04-02 17:46:48 -0300

    Criação de bean para usuário método de login

---
+++ Podcast/src/main/java/com/ifpb/control/controllers/UsuarioBean.java
@@ -0,0 +1,55 @@
+package com.ifpb.control.controllers;
+
+import com.ifpb.model.dao.Exceptions.DataAccessException;
+import com.ifpb.model.dao.impl.UsuarioDaoImpl;
+import com.ifpb.model.dao.interfaces.UsuarioDao;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class UsuarioBean {
+
+    private UsuarioDao usuarioDao;
+
+    private String email;
+
+    private String senha;
+
+    @PostConstruct
+    public void init() {
+        usuarioDao = new UsuarioDaoImpl();
+    }
+
+    public void efetuarLogin(){
+
+        try {
+            if(usuarioDao.autenticarUsuario(email,senha)){
+                System.out.println("foi!");
+            }else{
+                System.out.println("não foi!");
+            }
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
