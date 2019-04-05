510136490daad421e9e2d19ae6c385a3ce1629d8
antonio <antoniofilhonerde@gmail.com>
2019-04-05 10:30:32 -0300

    Bean utilitário para armazenamento de diretório de upload de arquivos

--- Podcast/src/main/java/com/ifpb/control/controllers/FileBean.java
+++ Podcast/src/main/java/com/ifpb/control/controllers/FileBean.java
@@ -5,23 +5,51 @@ import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.nio.file.Path;
 
 @ManagedBean
 @ApplicationScoped
 public class FileBean {
 
-    private String uploadFilePath = ((HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest()).getContextPath();
+
+    private String uploadAudioPath;
+
+    private String uploadImagePath;
 
     @PostConstruct
     public void init(){
-
+        String realpath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("");
+        File filepath = new File(realpath);
+        Path p = filepath.toPath();
+        for(int i = 0;i<5;i++){
+            p = p.getParent();
+        }
+        File pathAudio = new File(p.toString()+"/audios");
+        File pathImagem = new File(p.toString()+"/imagens");
+        if(!pathAudio.exists()){
+            pathAudio.mkdirs();
+        }
+        if(!pathImagem.exists()){
+            pathImagem.mkdirs();
+        }
+        uploadAudioPath = pathAudio.getAbsolutePath();
+        uploadImagePath = pathImagem.getAbsolutePath();
     }
 
-    public String getUploadFilePath() {
-        return uploadFilePath;
+    public String getUploadAudioPath() {
+        return uploadAudioPath;
     }
 
-    public void setUploadFilePath(String uploadFilePath) {
-        this.uploadFilePath = uploadFilePath;
+    public void setUploadAudioPath(String uploadAudioPath) {
+        this.uploadAudioPath = uploadAudioPath;
+    }
+
+    public String getUploadImagePath() {
+        return uploadImagePath;
+    }
+
+    public void setUploadImagePath(String uploadImagePath) {
+        this.uploadImagePath = uploadImagePath;
     }
 }
