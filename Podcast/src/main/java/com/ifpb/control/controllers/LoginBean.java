1e31e6ddf1b50d65b0fd6e4ab907aeefec1443b0
Eduardo Lucas <killerstiev4@gmail.com>
2019-04-06 21:35:00 -0300

    modificado o javabean para salvar o usuário na sessão, para deslogar e para criar uma instância única de um login na sessão

--- Podcast/src/main/java/com/ifpb/control/controllers/LoginBean.java
+++ Podcast/src/main/java/com/ifpb/control/controllers/LoginBean.java
@@ -4,13 +4,17 @@ package com.ifpb.control.controllers;
 import com.ifpb.model.dao.Exceptions.DataAccessException;
 import com.ifpb.model.dao.impl.UsuarioDaoImpl;
 import com.ifpb.model.dao.interfaces.UsuarioDao;
+import com.ifpb.model.domain.Usuario;
+import org.apache.catalina.Session;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import java.io.IOException;
 
 @ManagedBean
 @SessionScoped
@@ -18,22 +22,44 @@ public class LoginBean {
 
     private UsuarioDao usuarioDao;
 
+    private Usuario user;
+
     private String email;
 
     private String senha;
 
+    private static LoginBean instance;
+
     @PostConstruct
     public void init() {
+        user = new Usuario();
         usuarioDao = new UsuarioDaoImpl();
+        instance = this;
     }
 
     public String efetuarLogin() throws DataAccessException {
-
-        if(!usuarioDao.autenticarUsuario(email,senha)){
+        user = usuarioDao.autenticarUsuario(email,senha);
+        if(user == null){
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login ou senha inválidos", "Login ou senha inválidos"));
             return "";
-        }return "home";
+        }
+        return "home";
 
+    }
+
+    public void logout()
+    {
+        this.user = null;
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+    }
+
+    public static LoginBean getInstance() throws Exception
+    {
+        if(instance == null)
+        {
+            throw new Exception("Não há usuario logado no sistema");
+        }
+        return instance;
     }
 
     public String getEmail() {
@@ -50,5 +76,13 @@ public class LoginBean {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public Usuario getUser() {
+        return user;
+    }
+
+    public void setUser(Usuario user) {
+        this.user = user;
     }
 }
