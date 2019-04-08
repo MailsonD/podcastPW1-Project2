6ffab4952ef1624b7b40714ff2cf4cbef9c40d6b
MailsonD <mailssondennis@gmail.com>
2019-04-07 23:31:51 -0300

    Upload de imagem e correção de servlet para buscar a imagem

--- Podcast/src/main/java/com/ifpb/control/controllers/UsuarioBean.java
+++ Podcast/src/main/java/com/ifpb/control/controllers/UsuarioBean.java
@@ -7,10 +7,25 @@ import com.ifpb.model.domain.Enum.NivelAcesso;
 import com.ifpb.model.domain.Usuario;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.servlet.http.Part;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 @ManagedBean
 @RequestScoped
@@ -23,6 +38,17 @@ public class UsuarioBean {
     private List<Usuario> usuarios;
 
     private List<Usuario> alunos;
+
+    private Part foto;
+
+    private Pattern pattern = Pattern.compile("image\\/.+");
+
+    @ManagedProperty("#{fileBean}")
+    private FileBean fileBean;
+
+    private Logger log = Logger.getLogger(UsuarioBean.class.getName());
+
+
 
     @ManagedProperty("#{loginBean}")
     private LoginBean loginBean;
@@ -86,6 +112,36 @@ public class UsuarioBean {
         }
     }
 
+    public void atualizarFoto(){
+        String nomeArquivo = Timestamp.from(Instant.now()).toString() + "-" + foto.getSubmittedFileName();
+        try (InputStream file = foto.getInputStream()){
+            Files.copy(file, new File(fileBean.getUploadImagePath() + "/" + nomeArquivo).toPath(), StandardCopyOption.REPLACE_EXISTING);
+            usuarioDao.salvarFoto(nomeArquivo,loginBean.getEmail());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void validateFile(FacesContext ctx, UIComponent comp, Object value) {
+        List<FacesMessage> msgs = new ArrayList<>();
+        Part file = (Part)value;
+        log.info(file.getContentType());
+        String type = file.getContentType().split("/")[0];
+        log.severe(type);
+        log.severe(""+file.getSize());
+        if (file.getSize() > 1024 * 5000) {
+            msgs.add(new FacesMessage("O arquivo é muito grande!"));
+        }
+        if (!pattern.matcher(file.getContentType()).matches()) {
+            msgs.add(new FacesMessage("O arquivo selecionado não é uma imagem!"));
+        }
+        if (!msgs.isEmpty()) {
+            throw new ValidatorException(msgs);
+        }
+    }
+
     public Usuario getUsuario() {
         return usuario;
     }
@@ -108,6 +164,22 @@ public class UsuarioBean {
 
     public void setAlunos(List<Usuario> alunos) {
         this.alunos = alunos;
+    }
+
+    public Part getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Part foto) {
+        this.foto = foto;
+    }
+
+    public FileBean getFileBean() {
+        return fileBean;
+    }
+
+    public void setFileBean(FileBean fileBean) {
+        this.fileBean = fileBean;
     }
 
     public LoginBean getLoginBean() {
